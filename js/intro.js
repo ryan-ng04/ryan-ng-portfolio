cea6f65f9179a4291ff53310e5b1dc82b077bcc8
ryan-ng04 <ryanmhng04@gmail.com>
2025-10-18 03:39:57 -0500

    added base intro

---
+++ js/intro.js
@@ -0,0 +1,37 @@
+function typing() {
+    const text = ["Hi, I'm Ryan 👋", "Aspiring Engineer 💻", "Big Eater 🍜"];
+    const fact = document.querySelector("#intro");
+    let i = 0;
+    let j = 0;
+    let current = text[0];
+    let isDeleting = false;
+
+
+    function type() {
+        if (!isDeleting && j < current.length) {
+        // typing forward
+            fact.textContent += current.charAt(j++);
+            setTimeout(type, 80);
+        } else if (isDeleting && j > 0) {
+        // deleting backward
+        fact.textContent = current.substring(0, --j);
+        setTimeout(type, 40);
+        } else {
+        // pause at end or start
+        if (!isDeleting && j === current.length) {
+            isDeleting = true;
+            setTimeout(type, 1200);
+        } else {
+            isDeleting = false;
+            i = (i + 1) % text.length;
+            current = text[i];
+            setTimeout(type, 300);
+        }
+        }
+    }
+    type();
+}
+  
+
+
+document.addEventListener("DOMContentLoaded", typing);
